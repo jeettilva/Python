68da9751b49ba362197d7f09c8d19bbb8d6835eb
Jeet Tilva <82746306+jeettilva@users.noreply.github.com>
2021-08-03 17:58:47 +0530

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,10 @@
+def add(a=12,b=16):
+    print(a+b)
+def subtractor(a=12,b=16):
+    print(a-b)
+def multiply(a=12,b=16):
+    print(a*b)
+def divide(a=12,b=16):
+    print(a/b)
+def power(a=2,b=4):
+    print(a**b)
